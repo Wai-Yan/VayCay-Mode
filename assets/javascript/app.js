535b9cd1524610d0fcbd7528666ca9f82beb75a3
Angela Kressin <angelakressin@Angelas-MacBook-Pro.local>
2018-01-13 10:45:53 -0500

    updating html, js and css

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+///
+// $("#card").flip({
+//   axis: 'y',
+//   trigger: 'click'
+// });
+
+/// this sets the current date for the date selector
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+ if(dd<10){
+        dd='0'+dd
+    }
+    if(mm<10){
+        mm='0'+mm
+    }
+
+today = yyyy+'-'+mm+'-'+dd;
+$(".calendar").attr("min", today);
+
+})
+
+/// end

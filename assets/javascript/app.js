3660ae64597bbeea1a5a3217df3d6afb5d1adf1f
Ali Kassam <alikassam0@gmail.com>
2018-01-14 14:52:37 -0500

    firebase initiated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,19 @@
 
 $(document).ready(function() {
 
+  //Initailize Firebase
+   var config = {
+    apiKey: "AIzaSyDFiW-XEMBCNpnjixW04WxSoybIbyvS9OY",
+    authDomain: "alitraintime-fa097.firebaseapp.com",
+    databaseURL: "https://alitraintime-fa097.firebaseio.com",
+    projectId: "alitraintime-fa097",
+    storageBucket: "",
+    messagingSenderId: "885151914772"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
 ///
 // $("#card").flip({
 //   axis: 'y',
@@ -24,20 +37,31 @@ today = yyyy+'-'+mm+'-'+dd;
 $(".calendar").attr("min", today);
 
 
-
 //function to autofill city name and retreive google data 
 var input = document.getElementById('cityInput');
 var autocomplete = new google.maps.places.Autocomplete(input, {types: ['(cities)']});
-google.maps.event.addListener(autocomplete, 'place_changed', function(){
- var place = autocomplete.getPlace();
-console.log(place);
-console.log("place is: " + place.formatted_address);
-})
+google.maps.event.addListener(autocomplete, 'place_changed', retrieveLocation)
+
+
+
+//function to retrieve destination from Google
+function retrieveLocation(){
+var place = autocomplete.getPlace();
+console.log(place)
+return place;
+}
+
+//function to render rows
+function renderRows (){
+  console.log("render rows works")
+  console.log(place.formatted_address)
+}
 
 //on click function when user clicks the add button 
-$(document).on("click", "#destinationSubmit", function(event){
+$(document).on("click", "#addTrip", function(event){
 	event.preventDefault();
 	console.log("button works");
+  renderRows(place)
 })
 
 //function to fill carousel
@@ -49,7 +73,7 @@ $("#addTrip").click(function() {
     }
 });
 
-});
+// });
 
 
 

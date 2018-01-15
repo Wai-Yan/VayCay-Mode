fc2fd39533df2669e78799adff63500440a78a3c
Wai Yan <wai.yan0011553@gmail.com>
2018-01-15 13:54:02 -0500

    Add weather work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,9 +23,9 @@ $(document).ready(function() {
 //function to render rows
 function renderRows (place){
     $(".tableRow").empty()
-    console.log("render rows works")
-    console.log("render rows: " + place.formatted_address)
-    console.log(place.formatted_address);
+    // console.log("render rows works")
+    // console.log("render rows: " + place.formatted_address)
+    // console.log(place.formatted_address);
     var tBody = $("tbody");
     var tRow = $("<tr>");
 
@@ -43,8 +43,8 @@ console.log("storeInputValues works")
 destination = place.formatted_address;
 var rawDestinationDate = $("#dateInput").val().trim();
 destinationDate = moment(rawDestinationDate).format('MM/DD/YYYY');
-console.log("destination is" + destination);
-console.log("destination date is: " + destinationDate);
+// console.log("destination is" + destination);
+// console.log("destination date is: " + destinationDate);
 }
 
 
@@ -52,8 +52,10 @@ console.log("destination date is: " + destinationDate);
 $(document).on("click", "#addTrip", function(event){
 	event.preventDefault();
   storeInputValues(retrieveLocation());
-	console.log("button works");
+	// console.log("button works");
   renderRows(retrieveLocation());
+  showCurrentWeather(retrieveLocation())
+  showForecastedWeather(retrieveLocation());
 })
   ///
   /// flip.js function is called here
@@ -90,7 +92,7 @@ $(document).on("click", "#addTrip", function(event){
   //function to retrieve destination from Google
   function retrieveLocation(){
     var place = autocomplete.getPlace();
-    console.log(place)
+    console.log(place);
     return place;
   }
 
@@ -107,7 +109,7 @@ $(document).on("click", "#addTrip", function(event){
   });
 
   function fillCarousel() {
-    console.log("lmao");
+    // console.log("lmao");
   }
 
   // function to generate and initiate clock countdown flip
@@ -123,6 +125,66 @@ $(document).on("click", "#addTrip", function(event){
     clock = $('.clock').FlipClock(diff, {
       clockFace: 'DailyCounter',
       countdown: true
+    });
+  };
+
+  function showCurrentWeather(userPlace) {
+
+    // console.log(userPlace);
+
+    var APIKey = "ef097988a11b755c604a7aad621cf60d";
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+      "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
+
+    // console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      // We store all of the retrieved data inside of an object called "response"
+      .done(function(response) {
+        // Log the queryURL
+        // console.log(queryURL);
+        // Log the resulting object
+        // console.log(response);
+        // Transfer content to HTML
+        // $("#userCity").html("<h1>" + response.name + " Weather Details</h1>");
+        // $("#userCity").text("Wind Speed: " + response.wind.speed);
+        // $("#userCity").text("Humidity: " + response.main.humidity);
+        $("#currentWeather").text("Temperature (F) " + response.main.temp);
+        var newImage = $("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon +".png'>");
+        $("#currentWeather").prepend(newImage);
+
+      });
+  };
+
+  function showForecastedWeather(userPlace) {
+
+    var newForecastImage, date;
+
+    var APIKey = "ef097988a11b755c604a7aad621cf60d";
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=fairfax,us&units=imperial&appid=" + APIKey;
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    })
+    // We store all of the retrieved data inside of an object called "response"
+    .done(function(response) {
+      console.log(response);
+
+      for (var i = 0; i < 6; i++) {
+        newForecastImage = $("<img src='http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png'>");
+        $("#forecastedWeather").append(newForecastImage);
+
+        date = (response.list[i].dt_txt).slice(0, 11);
+        console.log(date);
+
+        $("#forecastedWeather").append(date + "|" + response.list[i].main.temp + "Fahrenheit");
+      }
     });
   }
 });
